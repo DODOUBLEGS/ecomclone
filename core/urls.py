d910766d3198651a1e106f4694f8c68e01fffa45
nihal <105669697+DODOUBLEGS@users.noreply.github.com>
2024-01-20 01:05:30 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     #
    path('api/products/<str:shop_name>/', ProductListByShop.as_view(), name='product-list-by-shop'),
    path('api/products/<str:shop_name>/<str:category_name>/', ProductListByShopAndCategory.as_view(), name='product-list-by-shop-and-category'),
+ path('api/categories/<str:shop_name>/', CategoryListByShop.as_view(), name='category-list-by-shop'),
 
 
 ]
